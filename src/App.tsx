79e25726f7608e737532862f46250884ca213e57
oswald884 <dr-fa_tes@hotmail.com>
2024-06-11 21:04:45 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full h-dvh">
+      <div className="flex md:gap-10 w-11/12 h-[90dvh] items-center justify-center">
+        <div className="min-h-full bg-black ">Left Side Panel</div>
+        <div className="min-h-full flex-1 bg-black ">Middle Tasks Panel</div>
+        <div className="min-h-full bg-black ">Right Side Details Panel</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
